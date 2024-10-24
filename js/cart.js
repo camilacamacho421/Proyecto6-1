2dee049cf40edf26eefb1eccdf4eaad078f31d3e
camilacamacho <89651954+camilacamacho421@users.noreply.github.com>
2024-10-24 20:44:05 -0300

    producto en carrito

--- js/cart.js
+++ js/cart.js
@@ -7,22 +7,25 @@ const productoComprado = localStorage.getItem('productoComprado');
 // Verificar si existe algún producto
 if (!productoComprado) {
     let htmlContentToAppend = `
-        <p>No hay productos</p>
+        <h5>No hay productos</h5>
     `;
 
     // Insertar el contenido generado en el DOM, accediendo al primer elemento de la colección
     document.getElementsByClassName('texto_producto')[0].innerHTML = htmlContentToAppend;
 } else {
+    // Convertir el string almacenado en localStorage en un objeto
+    const producto = JSON.parse(productoComprado);
+
+    // Acceder a las propiedades del objeto
     let htmlContentToAppend = `
-        <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/1GrakTl.jpg"></div>
+        <div class="col-2"><img class="img-fluid" src="${producto.image}" alt="${producto.name}"></div>
         <div class="col">
-            <div class="row text-muted">Shirt</div>
-            <div class="row">Cotton T-shirt</div>
+            <div class="row text-muted">${producto.name}</div>
         </div>
         <div class="col">
-            <a class="border" href="#">-</a><a href="#" class="border">1</a><a class="border" href="#">+</a>
+            <a class="border" href="#">-</a><a href="#" class="border">${producto.quantity}</a><a class="border" href="#">+</a>
         </div>
-        <div class="col">&dollar; 44.00 <span class="close">&#10005;</span></div>
+        <div class="col">&dollar; ${producto.cost} <span class="close">&#10005;</span></div>
     `;
 
     // Insertar el contenido generado en el DOM, accediendo al primer elemento de la colección
