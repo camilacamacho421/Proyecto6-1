3fc212dbbd6fb79da15a063fe5c37788e6cdfd60
camilacamacho <89651954+camilacamacho421@users.noreply.github.com>
2024-10-24 20:29:20 -0300

    muestra que no hay productos

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,30 @@
-//FUNCIÓN EN COMÚN PARA REDIRIGIR (OPCIONES MENU DESPLEGABLE)
 document.addEventListener('DOMContentLoaded', () => {
     Desafiante();
 });
 
+const productoComprado = localStorage.getItem('productoComprado');
+
+// Verificar si existe algún producto
 if (!productoComprado) {
     let htmlContentToAppend = `
-                    No hay productos
-                `;
+        <p>No hay productos</p>
+    `;
 
-    // Insertar el contenido generado en el DOM
-    document.getElementsById('mostrarProductos').innerHTML = htmlContentToAppend;
-}
-
-else {
+    // Insertar el contenido generado en el DOM, accediendo al primer elemento de la colección
+    document.getElementsByClassName('texto_producto')[0].innerHTML = htmlContentToAppend;
+} else {
     let htmlContentToAppend = `
-    <div class="row main align-items-center">
-                        <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/1GrakTl.jpg"></div>
-                        <div class="col">
-                            <div class="row text-muted">Shirt</div>
-                            <div class="row">Cotton T-shirt</div>
-                        </div>
-                        <div class="col">
-                            <a class="border" href="#">-</a><a href="#" class="border">1</a><a class="border" href="#">+</a>
-                        </div>
-                        <div class="col">&dollar; 44.00 <span class="close">&#10005;</span></div>
-                    </div>
-                `;
-    
-                
+        <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/1GrakTl.jpg"></div>
+        <div class="col">
+            <div class="row text-muted">Shirt</div>
+            <div class="row">Cotton T-shirt</div>
+        </div>
+        <div class="col">
+            <a class="border" href="#">-</a><a href="#" class="border">1</a><a class="border" href="#">+</a>
+        </div>
+        <div class="col">&dollar; 44.00 <span class="close">&#10005;</span></div>
+    `;
 
-    // Insertar el contenido generado en el DOM
-    document.getElementsByClassName('row main align-items-center').innerHTML = htmlContentToAppend;
+    // Insertar el contenido generado en el DOM, accediendo al primer elemento de la colección
+    document.getElementsByClassName('texto_producto')[0].innerHTML = htmlContentToAppend;
 }
