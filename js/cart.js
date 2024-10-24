a67b8b8615de0581614ae37db760cb29f8cd7462
camilacamacho <89651954+camilacamacho421@users.noreply.github.com>
2024-10-24 20:10:24 -0300

    cambios

--- js/cart.js
+++ js/cart.js
@@ -2,3 +2,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     Desafiante();
 });
+
+if (!productoComprado) {
+    let htmlContentToAppend = `
+                    No hay productos
+                `;
+
+    // Insertar el contenido generado en el DOM
+    document.getElementsById('mostrarProductos').innerHTML = htmlContentToAppend;
+}
+
+else {
+    let htmlContentToAppend = `
+    <div class="row main align-items-center">
+                        <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/1GrakTl.jpg"></div>
+                        <div class="col">
+                            <div class="row text-muted">Shirt</div>
+                            <div class="row">Cotton T-shirt</div>
+                        </div>
+                        <div class="col">
+                            <a class="border" href="#">-</a><a href="#" class="border">1</a><a class="border" href="#">+</a>
+                        </div>
+                        <div class="col">&dollar; 44.00 <span class="close">&#10005;</span></div>
+                    </div>
+                `;
+    
+                
+
+    // Insertar el contenido generado en el DOM
+    document.getElementsByClassName('row main align-items-center').innerHTML = htmlContentToAppend;
+}
